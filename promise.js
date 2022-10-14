454f901503f96cfc0e99c07d01ee4e7af68aea87
irawan <irbasu@gmail.com>
2022-10-14 10:36:38 +0700

    belajar promise

--- promise.js
+++ promise.js
@@ -3,6 +3,15 @@ const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
 const promiseOutput = null;
 
+
+const marah = true;
+const ApakahMarah = new Promise((resolve, reject) => {
+     //if(marah)
+     // mrh=mrh+1;
+     //else
+     // tdkmrh=tdkmrh+1;
+})
+
 module.exports = {
   promiseOutput,
 };
